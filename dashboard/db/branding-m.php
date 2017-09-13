83c68927f1caf9616b8fb7a8db231372308e8ac3
DssboxSSA <31927293+DssboxSSA@users.noreply.github.com>
2017-09-14 00:01:16 +0100

    Update branding-m.php

--- dashboard/db/branding-m.php
+++ dashboard/db/branding-m.php
@@ -1,3 +1,3 @@
 <ul class="dropdown-menu pull-right">
-  <li style="border-top: 1px solid #444"><a href="seederror@dssbox.com" target="_blank"><i class="fa fa-warning text-warning"></i><?php echo T('ISSUE_REPORT_TXT'); ?></a></li>
+  <li style="border-top: 1px solid #444"><a href="https://report.seedboxes.dssbox.com/bug_report_page.php" target="_blank"><i class="fa fa-warning text-warning"></i><?php echo T('ISSUE_REPORT_TXT'); ?></a></li>
 </ul>
